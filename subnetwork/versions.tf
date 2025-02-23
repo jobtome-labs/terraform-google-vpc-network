37d2069f6efc07e7396ccef6985b4eaa0e6f224e
Daniel Țoncu <toncu.daniel@gmail.com>
2025-02-23 16:19:09 +0200

    Update Versions

--- subnetwork/versions.tf
+++ subnetwork/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.6.0"
+      version = "~> 6.21"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.6.0"
+      version = "~> 6.21"
     }
   }
 }
